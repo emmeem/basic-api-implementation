12480b0330222b0e81151866ae61c861878f3417
emmeem <1124782639@qq.com>
2020-08-04 22:19:41 +0800

    feat:getoneRsevent

--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -1,10 +1,25 @@
 package com.thoughtworks.rslist;
 
+import com.thoughtworks.rslist.api.RsController;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class RsListApplicationTests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void shouldGetOneRsEvent() throws Exception {
+        mockMvc.perform(get("/rs/1"));
+    }
 
     @Test
     void contextLoads() {
